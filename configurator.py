3636d007ca01cb0d45872e9fd97585ab0c416c10
Andro0o0o0w-Romany <anderwscropio69@gmail.com>
2024-05-22 19:33:32 +0000

    handling the duplicate entry error with the new feature match case this like switch case in other languages but in python called match case

---
+++ configurator.py
@@ -0,0 +1,8 @@
+
+configurator= [
+    {"doctype":"Item Group", "filters":[[]], "fields":["*"]},
+    {"doctype":"Price List", "filters":[[]], "fields":["*"]},
+    {"doctype":"Brand", "filters":[[]], "fields":["*"]},
+    {"doctype":"Item", "filters":[[]], "fields":["*"]},
+    #{"doctype":"Item Price", "filters":[[]], "fields":["*"]},   
+]
--- data_seeding_script.py
+++ data_seeding_script.py
@@ -1,39 +1,53 @@
 from frappeclient import FrappeClient
+from frappe import log_error
 from decouple import config # for protecting the credentials I used python-decouple
-
+from configurator import configurator
 # protecting the credentials
 #server 1
+server1 = str(input("Enter the source server name: "))
 server1_username = config("SERVER1_USERNAME")
 server1_password = config("SERVER1_PASSWORD")
 #server 2
+server2 = str(input("Enter the destination server name: "))
 server2_username = config("SERVER2_USERNAME")
 server2_password = config("SERVER2_PASSWORD")
+# http://development.localhost:8000
+# http://development2.localhost:8000
+
 # openning connection with the server 1
-conn = FrappeClient("http://development.localhost:8000", server1_username, server1_password)
+conn = FrappeClient(server1, server1_username, server1_password)
 
 
 # openning connection with the server 2
-conn2 = FrappeClient("http://development2.localhost:8000", server2_username, server2_password)
+conn2 = FrappeClient(server2, server2_username, server2_password)
+
+def process_configurator():
+    result_set = []
+    for conf in configurator:
+        fetch_insert_data_set(conf, result_set)
+
+def fetch_insert_data_set(conf, result_set):
+        documents = conn.get_list(conf["doctype"], fields=conf["fields"])             # getting the list of all the documents of all the doctypes
+        for doc in documents:                             # looping through all the documents of all the doctypes
+            doc["doctype"] = conf["doctype"]                  # setting the doctype to it's type
+            doc_already_exists = conn2.get_value(conf["doctype"], {"name": doc["name"]})
+            doctype = conn2.get_doc(conf["doctype"], doc["name"]).get("doctype")                        # getting the doctype of the document
+            doc_already_exists_name = conn2.get_value(conf["doctype"], {"name": doc["name"]})         # getting the name of the document that already exists
+            match not doc_already_exists:                                # handling the duplicate documents with Match Case this feature is new in python 3.10 
+                  case False:                   # False here means if the document already exists
+                        print(f"The document --{doctype}-- that you tried to insert already exists {doc_already_exists_name} in the destination server. Skipping")   # printing the message that the document already exists
+                  case True:                  # True here means if the document does not exist
+                        print(conn2.insert(doc))
+            result_set.append({"Doctype": doc["doctype"], "name": doc["name"]})   # appending the doctype and the name of the document to the result_set
+        
 
 
-# getting item from the server 1
-item_list = conn.get_list("Item", fields=["name"])
-print(item_list)
-print("=======================================================================================================================================================================")
-item = conn.get_doc("Item", "SKU001")
-print(item)
-print("=======================================================================================================================================================================")
-# getting item filtered with item_group from the server 1
-item_group = conn.get_list("Item", filters={"item_group": "Demo Item Group"}, fields=["name"])
-print(item_group)
-print("=======================================================================================================================================================================")
-print("=======================================================================================================================================================================")
 
-#getting item price from the server 1
-item_price_list = conn.get_list("Item Price")
-print(item_price_list)
-print("=======================================================================================================================================================================")
-print("=======================================================================================================================================================================")
 
-item_price_list_by_brand = conn.get_list("Item Price", filters={"brand": "Demo Brand"}, fields=["name"])
-print(item_price_list_by_brand)
+
+
+
+if __name__ == "__main__":
+    process_configurator( )
+
+
